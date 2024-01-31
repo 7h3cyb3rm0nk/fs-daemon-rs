f94b83e889a444d56805ff3122e3e219d0a0edf8
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-01-31 14:51:00 +0530

    feat(main.rs): add main fn add main() run_daemon() to run the daemon to watch specific directory and handle_event() to handle the events while the daemon is runnning

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use notify::{RecommendedWatcher, Watcher, RecursiveMode, DebouncedEvent};
+use std::sync::mpsc::{channel, Receiver};
+use std::time::Duration;
+
+use notify_rust::{Notification, Urgency};
+
+mod file_watcher;
+mod notifier;
+
+fn main() {
+    if let Err(err) = run_daemon() {
+        eprintln!("Error: {}", err);
+    }
+}
+
+
+
+fn run_daemon() -> Result<(), Box<dyn std::error::Error>> {
+    let mut watcher = file_watcher::FileWatcher::new()?;
+    watcher.watch_directory("/home");
+    watcher.watch_directory("/opt/");
+
+
+    loop {
+        match watcher.receive_event() {
+            Ok(event) => handle_event(event),
+            Err(err) => eprintln!("Watcher error: {}", err),
+        }
+    }
+}
+
+
+fn handle_event(event: DebouncedEvent) {
+    match event {
+        DebouncedEvent::Create(path) => {
+            notifier::send_notification("File Created", &path.to_string_lossy());   
+        }
+
+        DebouncedEvent::Write(path) => {
+            notifier::send_notification("File Written", &path.to_string_lossy());
+        }
+        
+        DebouncedEvent::Remove(path) => {
+            notifier::send_notification("File Deleted", &path.to_string_lossy());
+        }
+
+        DebouncedEvent::Rename(old_path, new_path) => {
+            notifier::send_notification("File Moved", &format!("{} to {}", old_path.to_string_lossy(), new_path.to_string_lossy()));
+        }
+
+        DebouncedEvent::NoticeAccess(path) => {
+            notifier::send_notification("File Accessed", &path.to_string_lossy());
+        }
+
+        DebouncedEvent::NoticeModify(path) => {
+            notifier::send_notification("File Modified", &path.to_string_lossy());
+        }
+        _ => {}
+
+    }
+}
